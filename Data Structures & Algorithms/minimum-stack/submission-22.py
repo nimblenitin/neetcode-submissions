248df16d70e4537ecc2a5caece1306994f67d794
Nitin Naidu <naidunitin1@gmail.com>
2026-07-17 14:18:06 -0400

    Add: minimum-stack - submission-22

---
+++ Data Structures & Algorithms/minimum-stack/submission-22.py
@@ -0,0 +1,20 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minS = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        self.minS.append(self.minS[-1] if self.minS and self.minS[-1] < val else val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minS.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.minS[-1]
