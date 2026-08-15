ccdd1f44a2dcf263924ed4b654484439f853be1c
Nitin Naidu <naidunitin1@gmail.com>
2026-08-15 16:36:28 -0400

    Add: house-robber-iii - submission-5

---
+++ Data Structures & Algorithms/house-robber-iii/submission-5.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        
+        def dfs(node):
+            if not node:
+                return [0, 0]
+            
+            lCalc = dfs(node.left)
+            rCalc = dfs(node.right)
+            withRoot = node.val + lCalc[1] + rCalc[1]
+            withoutRoot = max(lCalc) + max(rCalc)
+            return [withRoot, withoutRoot]
+        
+        return max(dfs(root))
