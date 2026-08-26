a631095ea3da9321ef1a197edbdceefb4bc02392
Nitin Naidu <naidunitin1@gmail.com>
2026-08-26 14:51:19 -0400

    Add: subarray-sum-equals-k - submission-7

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-7.py
@@ -0,0 +1,11 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        totC = {0:1}
+        ways = 0
+        tot = 0
+        for n in nums:
+            tot += n
+            if (tot - k) in totC:
+                ways += totC[(tot - k)]
+            totC[tot] = totC.get(tot, 0) + 1
+        return ways
